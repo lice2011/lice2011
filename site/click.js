b8f27595c4d4b66ff256477e21fc336f28c5e81a
lice2011 <117921765+lice2011@users.noreply.github.com>
2022-11-10 11:28:56 -0300

    Add files via upload

---
+++ site/click.js
@@ -0,0 +1,10 @@
+function umClique(){
+  document.getElementById("imagem").src="826a1a26-ee43-47dc-97bc-f6cbdb06d6db.jpg";
+  
+}
+
+
+function doisCliques(){
+  document.getElementById("imagem").src="546fc431-d0d4-434f-95ba-223b4bdd0136.jpg";
+  
+}
